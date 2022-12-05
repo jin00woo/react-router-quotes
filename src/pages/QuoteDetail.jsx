c7409e4a2cad3391466ec1ad33320871bad2dba0
Jinwoo Han <56533546+jin00woo@users.noreply.github.com>
2022-12-05 14:50:53 +0000

    Created sorting button.

--- src/pages/QuoteDetail.jsx
+++ src/pages/QuoteDetail.jsx
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
@@ -9,6 +9,7 @@ const DUMMY_QUOTES = [
 ];
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
 
   const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
@@ -21,7 +22,12 @@ const QuoteDetail = () => {
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author}>
       </HighlightedQuote>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Route path={match.path} exact>
+        <div className="centered">
+        <Link to={`${match.url}/comments`} className="btn--flat">Load Comments</Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
         <Comments></Comments>
       </Route>
     </Fragment>);
